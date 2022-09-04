ec6672b5fdb8c5fc80d78d98a04205882352dd33
Gagandeep <gagandeepkaur1523@gmail.com>
2022-09-04 11:25:19 +0530

    Added bottom navigation bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import './home_page_content.dart';
+import './profile_page.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:line_icons/line_icons.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,13 +13,61 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  //list of screens for navigation bar
+  final screens = [
+    HomePageContent(),
+    ProfilePage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 40, 190, 172),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 40, 190, 172),
+        ),
+        body: Center(
+          child: screens[_selectedIndex],
+        ),
+
+        //Bottom Navigation Bar
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(color: Colors.white, boxShadow: [
+            BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+          ]),
+          child: SafeArea(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+              child: GNav(
+                  gap: 8,
+                  activeColor: Colors.white,
+                  iconSize: 24,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  duration: Duration(milliseconds: 300),
+                  tabBackgroundColor: Color.fromARGB(255, 40, 190, 172),
+                  tabs: [
+                    GButton(
+                      icon: LineIcons.home,
+                      text: 'Home',
+                    ),
+                    GButton(
+                      icon: LineIcons.user,
+                      text: 'Profile',
+                    ),
+                  ],
+                  selectedIndex: _selectedIndex,
+                  onTabChange: (index) {
+                    setState(() {
+                      _selectedIndex = index;
+                    });
+                  }),
+            ),
+          ),
+        ),
       ),
-    ));
+    );
   }
 }
