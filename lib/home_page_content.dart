11021214daec3478a88dfd237ecdda68437df5e5
Gagandeep <gagandeepkaur1523@gmail.com>
2022-09-04 11:34:49 +0530

    Added Grid View

--- lib/home_page_content.dart
+++ lib/home_page_content.dart
@@ -5,6 +5,31 @@ class HomePageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.count(
+      padding: const EdgeInsets.all(20),
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 20,
+      crossAxisCount: 2,
+      children: <Widget>[
+        Card(
+          child: Text('Card 1'),
+        ),
+        Card(
+          child: Text('Card 2'),
+        ),
+        Card(
+          child: Text('Card 3'),
+        ),
+        Card(
+          child: Text('Card 4'),
+        ),
+        Card(
+          child: Text('Card 5'),
+        ),
+        Card(
+          child: Text('Card 6'),
+        ),
+      ],
+    );
   }
 }
